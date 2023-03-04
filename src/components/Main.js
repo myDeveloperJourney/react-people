471d492242c455d5e787c8b53267974705f7a31c
Daniel JS <daniel@mydeveloperjourney.com>
2023-03-04 14:33:41 -0600

    wip: adds token to create people

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,31 +9,39 @@ function Main(props) {
   const API_URL = 'http://localhost:3001/people'
 
   const getPeople = async () => {
-    let token;
-
-    if(props.user) {
-      token = await props.user.getIdToken();
-      const response = await fetch(API_URL, {
-        method: 'GET',
-        headers: {
-          'Authorization': 'Bearer ' + token
-        }
-      });
-      const data = await response.json();
-      setPeople(data);
+    
+    try {
+      if(props.user) {
+        const token = await props.user.getIdToken();
+        const response = await fetch(API_URL, {
+          method: 'GET',
+          headers: {
+            'Authorization': 'Bearer ' + token
+          }
+        });
+        const data = await response.json();
+        setPeople(data);
+      }
+    } catch (error) {
+      // we can handle the error now
+      // ex. make modal pop up to tell user something went wrong
     }
   }
   
   const createPeople = async (person) => {
     try {
-      await fetch(API_URL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'Application/json',
-        },
-        body: JSON.stringify(person),
-      });
-      getPeople();
+      if(props.user) {
+        const token = await props.user.getIdToken();
+        await fetch(API_URL, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'Application/json',
+            'Authorization': 'Bearer ' + token
+          },
+          body: JSON.stringify(person),
+        });
+        getPeople();
+      }
     } catch (error) {
       // TODO: Add a task we wish to perform in the event of an error
     }
